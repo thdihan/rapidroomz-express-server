f53b5b3d152d821d0139356e24953a01d7199a88
thdihan <thdihan@outlook.com>
2026-07-15 22:31:26 +0600

    Cors issue fix

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import router from "./router";
 const app: Application = express();
 
 app.use(express.json());
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000", "https://dev.rapidroomz.com"];
 
 app.use(
     cors({
